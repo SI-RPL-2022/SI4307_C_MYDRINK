21e5dfde4a45c61c28fe30abbad9318bbd761ef6
chetakusuma <chetakus@student.telkomuniversity.ac.id>
2022-05-23 07:51:57 +0700

    Add: file manajemen user

---
+++ mydrink/resources/views/admin/user/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <span>
+                    User
+                </span>
+                <span>
+                    <a href="{{ route('user.index') }}" class="btn btn-primary">Kembali</a>
+                </span>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('user.store') }}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="">Nama</label>
+                        <input type="text" name="name"  class="form-control">
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">Email</label>
+                        <input type="email" name="email"  class="form-control">
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">Password</label>
+                        <input type="password" name="password"  class="form-control">
+                        @error('password')
+                            <div class="invalid-feedback">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">Role</label>
+                        <select name="role" id="" class="form-control">
+                            <option value="">Pilih Role</option>
+                            <option value="customer">Customer</option>
+                            <option value="admin">Admin</option>
+                        </select>
+                        @error('role')
+                            <div class="invalid-feedback">
+                                {{$message}}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group mt-2">
+                        <button class="btn btn-primary">Store</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
